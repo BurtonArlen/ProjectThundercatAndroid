bd180434f54188a792a5ec580e8f3594945e9dcc
arlen <burton.arlen.1990@gmail.com>
2017-07-21 11:26:05 -0700

    bare bones activities created

---
+++ app/src/main/java/com/inboundrx/thundercatmki/DurationRewardActivity.java
@@ -0,0 +1,13 @@
+package com.inboundrx.thundercatmki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DurationRewardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duration_reward);
+    }
+}
